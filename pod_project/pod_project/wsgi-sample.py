084f68a387aa374d7e653b1e654e0ed7b463d991
Ptitloup <nicolas.can@univ-lille1.fr>
2015-06-02 17:26:01 +0200

    remove root folder in fixtures and change wsgi file to remove unused function

--- pod_project/pod_project/wsgi-sample.py
+++ pod_project/pod_project/wsgi-sample.py
@@ -8,21 +8,8 @@ https://docs.djangoproject.com/en/1.7/howto/deployment/wsgi/
 """
 
 import os
-import sys
-import site
-
-# Add the site-packages of the chosen virtualenv to work with
-site.addsitedir('~/.virtualenvs/django_pod/local/lib/python2.7/site-packages')
-
-# Add the app's directory to the PYTHONPATH
-sys.path.append('/usr/local/django_projects/pod/pod_project')
-sys.path.append('/usr/local/django_projects/pod/pod_project/pod_project')
-
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "pod_project.settings")
-
-# Activate your virtual env
-activate_env=os.path.expanduser("~/.virtualenvs/django_pod/bin/activate_this.py")
-execfile(activate_env, dict(__file__=activate_env))
 
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
+
