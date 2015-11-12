b9835c8341d6d33d396422e5ac1188cf251aeb05
Philippe Pomédio <philippe.pomedio@unice.fr>
2015-11-12 17:08:16 +0100

    Moved related js code to “main.js” in POD namespace

--- pod_project/core/static/JS/main.js
+++ pod_project/core/static/JS/main.js
@@ -1,3 +1,4 @@
+
 $(document).ready(function() {
     // Lancement de la recherche au clic sur le bouton de recherche
     $(document.body).on(
@@ -10,3 +11,42 @@ $(document).ready(function() {
         }
     );
 });
+
+
+
+( function( POD, $, undefined ) { 'use strict';
+
+
+    /*
+        Video creation / edition:
+
+            - selecting “Draft” hides “Restricted access” and “Password”
+
+    */
+    POD.setVideoEditForm = function( ) {
+
+        // Objects involved
+        var $isDraftCheckbox = $( '#id_is_draft' );
+        var $isRestrictedCheckboxGroup = $( '#id_is_restricted' ).closest( '.form-group' );
+        var $passwordTextInputGroup = $( '#id_password' ).parent( '.form-group' );
+
+        // Initial state
+        if ( $isDraftCheckbox.is( ':checked' ) ) {
+            $isRestrictedCheckboxGroup.hide( );
+            $passwordTextInputGroup.hide( );
+        }
+
+        // Dynamic update
+        $isDraftCheckbox.on( 'change', function( event ) {
+            if ( $( this ).is( ':checked' ) ) {
+                $passwordTextInputGroup.slideUp( );
+                $isRestrictedCheckboxGroup.slideUp( );
+            } else {
+                $isRestrictedCheckboxGroup.slideDown( );
+                $passwordTextInputGroup.slideDown( );
+            }
+        } );
+    };
+
+
+} ( window.POD = window.POD || { }, jQuery ) );
