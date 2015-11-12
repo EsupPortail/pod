1c971afad272b2f5d1e18c69e623170f0431bce2
Philippe Pomédio <philippe.pomedio@unice.fr>
2015-11-12 17:31:56 +0100

    Change help texts…
    …  for better user comprehension (maybe).

--- pod_project/pods/migrations/0004_AlterFields.py
+++ pod_project/pods/migrations/0004_AlterFields.py
@@ -43,7 +43,19 @@ class Migration(migrations.Migration):
         migrations.AlterField(
             model_name='pod',
             name='is_draft',
-            field=models.BooleanField(default=True, help_text='If you check this box, the video will be visible and accessible only by you.', verbose_name='Draft'),
+            field=models.BooleanField(default=True, help_text='If this box is checked, the video will be visible and accessible only by you.', verbose_name='Draft'),
+            preserve_default=True,
+        ),
+        migrations.AlterField(
+            model_name='pod',
+            name='is_restricted',
+            field=models.BooleanField(default=False, help_text='If this box is checked, the video will only be accessible to authenticated users.', verbose_name='Restricted access'),
+            preserve_default=True,
+        ),
+        migrations.AlterField(
+            model_name='pod',
+            name='password',
+            field=models.CharField(help_text='The video will be available with the specified password.', max_length=50, null=True, verbose_name='password', blank=True),
             preserve_default=True,
         ),
         migrations.AlterField(
