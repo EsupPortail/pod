5e682debe467379461ea39828b8c2bc44f8a68d8
Ptitloup <nicolas.can@univ=lille1.fr>
2016-09-20 12:33:58 +0200

    fix rest framework

--- pod_project/pod_project/settings-sample.py
+++ pod_project/pod_project/settings-sample.py
@@ -150,7 +150,7 @@ CACHES = {
 # WEBservices with rest API
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
-        'rest_framework.authentication.TokenAuthentication'
+        'rest_framework.authentication.TokenAuthentication',
     ),
     'DEFAULT_PERMISSION_CLASSES': (
         'rest_framework.permissions.IsAdminUser',
