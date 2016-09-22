dfaecb01e3a7c50320f5f8dd32ad54d90738032e
Philippe Pomédio <philippe.pomedio@unice.fr>
2016-09-22 13:30:37 +0200

    Adds an ARIA role on the link.

--- pod_project/core/static/js/player.js
+++ pod_project/core/static/js/player.js
@@ -292,6 +292,7 @@ function loadVideo() {
 
             logoLink.setAttribute( 'href', player_logo_url );
             logoLink.setAttribute( 'title', player_logo_title );
+            logoLink.setAttribute( 'role', "link" );
             logoLink.setAttribute( 'style', "font-size: 1.5em; font-weight: bold; line-height: 1.9em;" );
             logoImg.setAttribute( 'src', player_logo_img );
             logoImg.setAttribute('alt', player_logo_alt);
