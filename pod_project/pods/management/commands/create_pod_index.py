6ae28b22487c63ea188a702016160b5060472191
Philippe Pomédio <philippe.pomedio@unice.fr>
2016-10-19 17:22:48 +0200

    PEP8

--- pod_project/pods/management/commands/create_pod_index.py
+++ pod_project/pods/management/commands/create_pod_index.py
@@ -20,7 +20,7 @@ class Command(BaseCommand):
             create = es.indices.create(index='pod', body=es_template)  # ignore=[400, 404]
         except TransportError as e:
             # (400, u'IndexAlreadyExistsException[[pod] already exists]')
-            if e.status_code==400:
+            if e.status_code == 400:
                 print "l'index Pod est existant : %s" % e.error
             else:
                 print "Une erreur est survenue lors de la creation de l'index : %s-%s" % (e.status_code, e.error)
--- pod_project/pods/management/commands/index_videos.py
+++ pod_project/pods/management/commands/index_videos.py
@@ -8,6 +8,7 @@ import json
 
 ES_URL = getattr(settings, 'ES_URL', ['http://127.0.0.1:9200/'])
 
+
 class Command(BaseCommand):
     args = '__ALL__ or <pod_id pod_id ...>'
     help = 'Index in elasticsearch the specified video'
@@ -18,19 +19,19 @@ class Command(BaseCommand):
 
         es = Elasticsearch(ES_URL)
         if args:
-            if args[0]=='__ALL__':
+            if args[0] == '__ALL__':
                 delete = es.indices.delete(index='pod', ignore=[400, 404])
-                #delete = es.delete_by_query(index="pod", doc_type='pod', body={"query":{"match_all":{}}})
+                # delete = es.delete_by_query(index="pod", doc_type='pod', body={"query":{"match_all":{}}})
                 json_data = open('pods/search_template.json')
                 es_template = json.load(json_data)
                 try:
-                    create = es.indices.create(index='pod', body=es_template) #ignore=[400, 404]
+                    create = es.indices.create(index='pod', body=es_template)  # ignore=[400, 404]
                 except TransportError as e:
-                        # (400, u'IndexAlreadyExistsException[[pod] already exists]')
-                    if e.status_code==400:
-                        print "l'index Pod est existant : %s" %e.error
+                    # (400, u'IndexAlreadyExistsException[[pod] already exists]')
+                    if e.status_code == 400:
+                        print "l'index Pod est existant : %s" % e.error
                     else:
-                        print "Une erreur est survenue lors de la creation de l'index : %s-%s" %( e.status_code, e.error)
+                        print "Une erreur est survenue lors de la creation de l'index : %s-%s" % (e.status_code, e.error)
                 from pods.views import VIDEOS
                 for pod in VIDEOS:
                     res = es.index(index="pod", doc_type='pod', id=pod.id, body=pod.get_json_to_index(), refresh=True)
@@ -42,7 +43,7 @@ class Command(BaseCommand):
                         raise CommandError('Pod "%s" does not exist' % pod_id)
                     res = es.index(index="pod", doc_type='pod', id=pod.id, body=pod.get_json_to_index(), refresh=True)
         else:
-            print "******* Warning : you must give some arguments : %s *******" %self.args
+            print "******* Warning : you must give some arguments : %s *******" % self.args
 
 
 """
