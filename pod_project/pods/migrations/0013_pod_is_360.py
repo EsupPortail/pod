8d6482cb1088847737be69f34783b39c89825699
ppomedio <philippe.pomedio@unice.fr>
2016-12-22 15:18:04 +0100

    Reordering migration files
    One migration file was previously given the number 0012 since PR#181 in
    branch « master ».

--- pod_project/pods/migrations/0013_pod_is_360.py
+++ pod_project/pods/migrations/0013_pod_is_360.py
@@ -7,7 +7,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('pods', '0011_new_contributorpod'),
+        ('pods', '0012_default_contributor'),
     ]
 
     operations = [
